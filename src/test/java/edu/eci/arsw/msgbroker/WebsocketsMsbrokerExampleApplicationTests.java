11ff6e24f04e83ceed990b5cd19c7d309100a810
dcifuentesr <danixbox1313@gmail.com>
2019-04-24 13:33:00 -0500

    arregle problemas(exception) en las pruebas

--- src/test/java/edu/eci/arsw/msgbroker/WebsocketsMsbrokerExampleApplicationTests.java
+++ src/test/java/edu/eci/arsw/msgbroker/WebsocketsMsbrokerExampleApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import edu.eci.arsw.collabpaint.CollabPaintApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=CollabPaintApplication.class)
 public class WebsocketsMsbrokerExampleApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Test
+	public void contextLoads() {
+	}
 
 }
